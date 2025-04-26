a795b4a4da9c7c901331a5b31e1d13dba4fc1852
FiloCara <fili.cara@gmail.com>
2025-04-26 12:35:50 -0400

    Remove automatic software install

--- Readme.md
+++ Readme.md
@@ -1,3 +1,12 @@
 ```bash
 curl -Ls https://raw.githubusercontent.com/FiloCara/dotfiles/main/bootstrap.sh | bash 
 ```
+
+### Requirements
+
+- `git`
+- `zsh`
+- `nvim`
+- `tmux`
+- `fzf` (optional)
+- `exa` (optional)
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,28 +1,7 @@
 #!/bin/bash
+set -e
 
 DOTFILES_REPO="https://github.com/FiloCara/dotfiles.git"
-DOTFILES_DIR="$HOME/dotfiles"
-
-install_software() {
-    local software_name=$1
-    local install_cmd=$2
-
-    # Check if the software is not already installed
-    if ! command -v $software_name &> /dev/null; then
-        read -p "Do you want to install $software_name? (y/n) " answer </dev/tty
-        echo 
-        case $answer in
-            [Yy]* )
-                echo "Installing $software_name..."
-                eval $install_cmd
-                ;;
-            * )
-                echo "$software_name will not be installed."
-                ;;
-        esac    
-    else "$software_name is already installed."
-    fi
-}
 
 echo "Checking for prerequisites..."
 if ! command -v git &> /dev/null; then
@@ -30,22 +9,17 @@ if ! command -v git &> /dev/null; then
     exit 1
 fi
 
-# Clone the dotfiles repository if it doesn't exist
-if [ ! -d "$DOTFILES_DIR" ]; then
-    echo "Cloning dotfiles repository to $DOTFILES_DIR"
-    git clone $DOTFILES_REPO $DOTFILES_DIR
+TARGET_DIR="$(pwd)/dotfiles"
+
+if [ ! -d "$TARGET_DIR" ]; then
+    echo "Cloning dotfiles repository into $TARGET_DIR"
+    git clone "$DOTFILES_REPO" "$TARGET_DIR"
 else
-    echo "Dotfiles repository already exists at $DOTFILES_DIR"
+    echo "Dotfiles repository already exists at $TARGET_DIR"
     exit 1
 fi
 
-# Execute the install script from the dotfiles repository
 echo "Running install script..."
-bash "$DOTFILES_DIR/install.sh"
+bash "$TARGET_DIR/install.sh"
 
-echo "Dotfiles setup completed."
-
-install_software "exa"  "sudo apt-get install -y exa"
-install_software "nvim" "sudo apt-get install -y neovim"
-install_software "zsh"  "sudo apt-get install -y zsh"
-install_software "tmux" "sudo apt-get install -y tmux"
+echo "✅ Dotfiles setup completed."
